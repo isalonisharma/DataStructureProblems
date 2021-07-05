297552f17e3c904d65164fad9dc443402108159c
SaloniSharma0809 <67950658+SaloniSharma0809@users.noreply.github.com>
2021-07-05 10:59:05 +0530

    #stack

--- Stack/src/problems/NearestGreaterElementLeftSide.java
+++ Stack/src/problems/NearestGreaterElementLeftSide.java
@@ -5,15 +5,15 @@ import java.util.Stack;
 public class NearestGreaterElementLeftSide {
 	public static void main(String[] args) {
 		int arr[] = { 21, 3, 13, 11 };
-		int result[] = printNGE(arr);
+		int output[] = printNGE(arr);
 		for (int i = 0; i < arr.length; i++) {
-			System.out.println(arr[i] + ": " + result[i]);
+			System.out.println(arr[i] + ": " + output[i]);
 		}
 	}
 
 	private static int[] printNGE(int[] arr) {
 		Stack<Integer> stack = new Stack<>();
-		int[] result = new int[arr.length];
+		int[] output = new int[arr.length];
 		int current;
 		for (int i = 0; i < arr.length; i++) {
 			current = arr[i];
@@ -25,9 +25,9 @@ public class NearestGreaterElementLeftSide {
 				stack.pop();
 			}
 			/* if stack is empty -1 or element found then save in result array */
-			result[i] = (stack.empty()) ? (-1) : (stack.peek());
+			output[i] = (stack.empty()) ? (-1) : (stack.peek());
 			stack.push(current);
 		}
-		return result;
+		return output;
 	}
 }
--- Stack/src/problems/NearestSmallerElementLeftSide.java
+++ Stack/src/problems/NearestSmallerElementLeftSide.java
@@ -5,24 +5,24 @@ import java.util.Stack;
 public class NearestSmallerElementLeftSide {
 	public static void main(String[] args) {
 		int arr[] = { 11, 13, 3, 21 };
-		int result[] = printNSE(arr);
+		int output[] = printNSE(arr);
 		for (int i = 0; i < arr.length; i++) {
-			System.out.println(arr[i] + ": " + result[i]);
+			System.out.println(arr[i] + ": " + output[i]);
 		}
 	}
 
 	private static int[] printNSE(int[] arr) {
 		Stack<Integer> stack = new Stack<>();
-		int[] result = new int[arr.length];
+		int[] output = new int[arr.length];
 		int current;
 		for (int i = 0; i < arr.length; i++) {
 			current = arr[i];
 			while (!stack.isEmpty() && current < stack.peek()) {
 				stack.pop();
 			}
-			result[i] = (stack.empty()) ? (-1) : (stack.peek());
+			output[i] = (stack.empty()) ? (-1) : (stack.peek());
 			stack.push(current);
 		}
-		return result;
+		return output;
 	}
 }
